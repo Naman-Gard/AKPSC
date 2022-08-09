a6f0a6fe4e0729dbb184f674fb9a5e3befd4870b
ujjwal272 <ujjwal.singh@netprophetsglobal.com>
2022-08-09 16:21:19 +0530

    ui

--- resources/views/step-form/form-head/index.blade.php
+++ resources/views/step-form/form-head/index.blade.php
@@ -1,8 +1,13 @@
-<section class="form-head">
+<section class="form-head ">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                
+                <ul id="progressbar">
+                    <li class="active" id="account"><strong>Education</strong></li>
+                    <li id="personal"><strong>Experience</strong></li>
+                    <li id="payment"><strong>Work Preference</strong></li>
+                    <li id="confirm"><strong>Upload</strong></li>
+                </ul>
             </div>
         </div>
     </div>
